d39fb85de2e6af53a7fc19cdb59379bce74eaa2d
MishaP <MishaP635@gmail.com>
2025-09-22 22:00:13 +0300

    edit faker user.data.js

--- less14/src/data/user.data.js
+++ less14/src/data/user.data.js
@@ -1,8 +1,11 @@
 // "базовий" користувач
-
+import { faker } from '@faker-js/faker';
 export const baseUser = {
-  name: 'John',
-  lastName: 'Dou',
+  name: faker.person.firstName(),          // випадкове ім’я
+  lastName: faker.person.lastName(),       // випадкове прізвище
   password: 'Qwerty12345',
   repeatPassword: 'Qwerty12345',
 };
+
+
+
